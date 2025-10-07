4e4cf8f6acb6bb20bbc9f1bfd4c5142f7361a658
Simran Kaur <simran292002@gmail.com>
2025-10-07 12:49:06 +0530

    changes

---
+++ FoodApp/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const AppLayout = () => {
+  return <div className="app">Hello</div>;
+};
+root.render(<AppLayout />);
